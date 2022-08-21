068ad78cbd66d053fcacfbb051f09cb4f8d560d1
healerit101 <healerit101@gmail.com>
2022-08-21 13:07:13 +0700

    fix error

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,13 +5,13 @@ export default function Header() {
   return (
     <>
       <header className='w-100'>
-        <nav class="container navbar navbar-expand-lg navbar-light ">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
+        <nav className="container navbar navbar-expand-lg navbar-light ">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
                 <Image src='https://cdn.popsww.com/popsapp/assets/images/icons/logo-pops.png?format=webp' width={120} height={40} alt="logo" />
             </a>
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -19,23 +19,23 @@ export default function Header() {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
                     Trang Chủ
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     Anime Mới
                   </a>
                 </li>
-                <li class="nav-item dropdown">
+                <li className="nav-item dropdown">
                   <a
-                    class="nav-link dropdown-toggle"
+                    className="nav-link dropdown-toggle"
                     href="#"
                     id="navbarDropdown"
                     role="button"
@@ -44,25 +44,25 @@ export default function Header() {
                     Anime Hot
                   </a>
                 </li>
-                <li class="nav-item">
+                <li className="nav-item">
                   <a
-                    class="nav-link disabled"
+                    className="nav-link disabled"
                     href="#"
-                    tabindex="-1"
+                    tabIndex="-1"
                     aria-disabled="true"
                   >
                     Anime Hay
                   </a>
                 </li>
               </ul>
-              <form class="d-flex search-header">
+              <form className="d-flex search-header">
                 <input
-                  class="form-control me-2"
+                  className="form-control me-2"
                   type="search"
                   placeholder="Tên phim"
                   aria-label="Search"
                 />
-                {/* <button class="btn btn-outline-success" type="submit">
+                {/* <button className="btn btn-outline-success" type="submit">
                   Search
                 </button> */}
               </form>
