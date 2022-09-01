1c048c062f630a5c77cfc4f5dbb0cb57fbee160b
healerit101 <healerit101@gmail.com>
2022-09-01 10:12:02 +0700

    fix seo onpage

--- pages/[slug].js
+++ pages/[slug].js
@@ -8,7 +8,19 @@ export default function FilmDetail({data}) {
     return (
     <>
         <Head>
-            
+          <title>{data.data.movie.name} </title>
+          <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"></meta>
+          {/* <meta name="description" content={data.data.pageProps.data.seoOnPage.descriptionHead} /> */}
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+          <meta property="og:title" content={data.data.movie.name}></meta>
+          <meta property="og:type" content="website"></meta>
+          {/* <meta property="og:description" content={data.data.pageProps.data.seoOnPage.descriptionHead}></meta> */}
+          <meta property="og:url" content={`${process.env.url_home}`}></meta>
+          <meta property="og:site_name" content="PHIMHAYHD.club"></meta>
+          <meta name="twitter:title" content={data.data.movie.name}></meta>
+          {/* <meta name="twitter:description" content={data.data.pageProps.data.seoOnPage.descriptionHead}></meta> */}
+          <meta property="og:locale" content="vi_VN"></meta>
+          <link rel="icon" href="/phimhaydd.png" />
         </Head>
         <Header />  
         <PostFilm filmDetail = {data} />
